4fe4b7bb63934aea7398f1c68b96e759fdfa8b30
Jonatan Streith <jonatan@streith.se>
2018-09-04 13:07:12 +0200

    Test

---
+++ WebApplication1/Models/VM_artikel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Datamanager_Mockup.Models
+{
+    public class VM_artikel
+    {
+        int a01TknKey;
+        string ArticleNumber;
+        int Category;
+        int Characteristics;
+        int Stock;
+        int OptimalStock;
+        int OrderRow;
+    }
+}
